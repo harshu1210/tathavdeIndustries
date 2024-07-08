98de96bd4bd3eb0ccae141b3cea20c17c1903091
harshu1210 <charshal40@gmail.com>
2024-07-08 20:04:24 +0530

    Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
   firebaseConfig :{
     apiKey: "AIzaSyCQOIYvEboCkw7cEWKDJSU0S9urxiedJYw",
     authDomain: "furnitureindustries-a34d2.firebaseapp.com",

85bed4836f4ffacf3f32c2c58f36ec1c18d9877f
harshu1210 <charshal40@gmail.com>
2024-07-11 00:35:25 +0530

    normal Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig :{
-    apiKey: "AIzaSyCQOIYvEboCkw7cEWKDJSU0S9urxiedJYw",
-    authDomain: "furnitureindustries-a34d2.firebaseapp.com",
-    projectId: "furnitureindustries-a34d2",
-    storageBucket: "furnitureindustries-a34d2.appspot.com",
-    messagingSenderId: "145560759067",
-    appId: "1:145560759067:web:5749671fe77b4b4756c1be",
-    measurementId: "G-QSQZ38LNVP"
-  },
+
 };
